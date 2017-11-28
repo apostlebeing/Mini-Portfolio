a47a958e7ff025194284e420313bf30adc5d26fb
codewilling <codewilling@gmail.com>
2017-11-27 22:30:53 -0800

    11/27 color game

--- colorGame/colorGame.js
+++ colorGame/colorGame.js
@@ -1,4 +1,5 @@
-let colors = generateRandomColors(6);
+let numSquares = 6;
+let colors = generateRandomColors(numSquares);
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
@@ -7,25 +8,53 @@ let h1 = document.querySelector("h1");
 let resetButton = document.querySelector("#reset");
 let easyBtn = document.querySelector("#easyBtn");
 let hardBtn = document.querySelector("#hardBtn");
+let modeButtons = document.querySelectorAll(".mode");
+
+for(var i = 0; i < modeButtons.length; i++){
+    modeButtons[i].addEventListener("click", function(){
+        
+    })
+}
 
 easyBtn.addEventListener("click", function(){
     hardBtn.classList.remove("selected");
     easyBtn.classList.add("selected");
-})
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        }else{
+            squares[i].style.display = "none"
+        }
+    }
+});
 
 hardBtn.addEventListener("click", function(){
     easyBtn.classList.remove("selected");
     hardBtn.classList.add("selected");
-})
-
-resetButton.addEventListener("click", function(){
-    colors = generateRandomColors(6);
+    numSquares = 6
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = "block";
+        }
+    });
+
+resetButton.addEventListener("click", function(){
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    this.textContent = "New Colors";
     for (let i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colors[i];
     };
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "steelblue";
+    messageDisplay.textContent = "";
 })
 
 colorDisplay.textContent = pickedColor;
